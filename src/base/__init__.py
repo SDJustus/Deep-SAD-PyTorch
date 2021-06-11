209a1da1e08972bc572f1248ce4114537026b0c6
SDJustus <justus.zipfel@tu-dresden.de>
2021-06-11 14:35:46 +0200

    added Custom_dataset for images

--- src/base/__init__.py
+++ src/base/__init__.py
@@ -3,3 +3,4 @@ from .torchvision_dataset import *
 from .odds_dataset import *
 from .base_net import *
 from .base_trainer import *
+from .custom_dataset import *
---
+++ src/datasets/custom.py
@@ -0,0 +1,86 @@
+from typing import Any, Callable, Optional, Tuple
+from torch.utils.data import Subset
+from torchvision.datasets.folder import default_loader
+from .preprocessing import create_semisupervised_setting
+from base.base_dataset import BaseADDataset
+from torch.utils.data import DataLoader
+from torchvision.datasets import DatasetFolder
+
+import torch
+import torchvision.transforms as transforms
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+class Custom_Dataset(BaseADDataset):
+
+    def __init__(self, root: str, ratio_known_normal: float = 0.0, ratio_known_outlier: float = 0.0, ratio_pollution: float = 0.0):
+        super().__init__(root)
+
+        # Define normal and outlier classes
+       # Define normal and outlier classes
+        self.n_classes = 2  # 0: normal, 1: outlier
+        self.normal_classes = (0,)
+        self.outlier_classes = (1,)
+        self.known_outlier_classes = (1,)
+            
+        transform = transforms.ToTensor()
+        target_transform = transforms.Lambda(lambda x: int(x in self.outlier_classes))
+
+        # Get train set
+        train_set = CustomDataset(root=self.root+"/train", transform=transform, target_transform=target_transform)
+
+        # Create semi-supervised setting
+        idx, _, semi_targets = create_semisupervised_setting(train_set.targets.cpu().data.numpy(), self.normal_classes,
+                                                             self.outlier_classes, self.known_outlier_classes,
+                                                             ratio_known_normal, ratio_known_outlier, ratio_pollution)
+        train_set.semi_targets[idx] = torch.tensor(semi_targets)  # set respective semi-supervised labels
+
+        # Subset train_set to semi-supervised setup
+        self.train_set = Subset(train_set, idx)
+
+        # Get test set
+        self.test_set = CustomDataset(root=self.root+"/test", transform=transform, target_transform=target_transform)
+        
+    def loaders(self, batch_size: int, shuffle_train=True, shuffle_test=False, num_workers: int = 0) -> (DataLoader, DataLoader):
+        train_loader = DataLoader(dataset=self.train_set, batch_size=batch_size, shuffle=shuffle_train,
+                                    num_workers=num_workers, drop_last=True)
+        test_loader = DataLoader(dataset=self.test_set, batch_size=batch_size, shuffle=shuffle_test,
+                                    num_workers=num_workers, drop_last=False)
+        return train_loader, test_loader
+
+
+
+class CustomDataset(DatasetFolder):
+    """
+    Custom Dataset for Deep-SAD
+
+    Dataset class with additional targets for the semi-supervised setting and modification of __getitem__ method
+    to also return the semi-supervised target as well as the index of a data sample.
+    """
+    
+    def __init__(self, root: str, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, loader: Callable[[str], Any] = default_loader, is_valid_file: Optional[Callable[[str], bool]] = None,):
+        super(CustomDataset, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                                          transform=transform,
+                                          target_transform=target_transform,
+                                          is_valid_file=is_valid_file)
+        self.imgs = self.samples
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any, Any, int]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target, semi_target, index) where target is class_index of the target class.
+        """
+        path, target = self.imgs[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target, int(torch.zeros_like(self.targets)[index]), index
+
+
+    def __len__(self) -> int:
+        return len(self.imgs)
